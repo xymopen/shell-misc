6c44735dbec49e65d7abc03092a3763fb514c3b5
Xu Yiming <xuyiming.open@outlook.com>
2019-11-21 10:34:47 +0800

    feat(*path): Split and concat strings

---
+++ frompath.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+if [ "$1" = '-h' -o "$1" = '--help' ]; then
+	cat - 1>& 2 <<- EOF
+		Usage: frompath [ VALUE ] [ DELIM ]
+
+		Split VALUE by DELIM
+		By default print directories in $PATH
+
+EOF
+else
+	IFS="${2:-:}"
+
+	for i in ${1:-$PATH}; do
+		echo "$i"
+	done
+fi
---
+++ topath.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+if [ "$1" = '-h' -o "$1" = '--help' ]; then
+	cat - 1>& 2 <<- EOF
+		Usage: topath [ DELIM ] [ FILE ]...
+
+		Concat elements from FILE by DELIM
+		By default print STDIN in $PATH format
+
+EOF
+else
+	delim="${1:-:}"
+	ret=""
+
+	for i in $(cat "${2:--}"); do
+		ret="${ret}${delim}${i}"
+	done
+
+	# Remove leading delimiter
+	echo "${ret#${delim}}"
+fi
